f42637059e427aa76665f1c12e7185f1961548e4
EyeSlash98 <rohanpj@rediffmail.com>
2018-03-24 16:49:06 +0530

    Half Way Done With The new Feature.

--- my-app.js
+++ my-app.js
@@ -1,5 +1,10 @@
-something() {
-	console.log('Iam someting');
+function aBigNewFeature() {
+	console.log('Writing a lot of new codes.');
+	console.log('Half Way done with this new feature.');
+}
+function something() {
+	console.log('Iam something');
 }
 console.log('Hello World');
 something();
+aBigNewFeature();
