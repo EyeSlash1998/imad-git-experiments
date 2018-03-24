02125b98a97fa302e84cf1db0faa66d05a47a553
EyeSlash98 <rohanpj@rediffmail.com>
2018-03-24 16:58:57 +0530

    New Feature Completed.

--- my-app.js
+++ my-app.js
@@ -1,6 +1,7 @@
 function aBigNewFeature() {
 	console.log('Writing a lot of new codes.');
 	console.log('Half Way done with this new feature.');
+	console.log('Finally Completely done with the new feature.');
 }
 function something() {
 	console.log('Iam something');
