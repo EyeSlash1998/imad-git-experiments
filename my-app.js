d09e9cf2357179cddb25c12dfed8df90dc2e7b6a
EyeSlash98 <rohanpj@rediffmail.com>
2018-03-25 18:08:01 +0530

    New Changes made.

--- my-app.js
+++ my-app.js
@@ -3,9 +3,12 @@ function aBigNewFeature() {
 	console.log('Half Way done with this new feature.');
 	console.log('Finally Completely done with the new feature.');
 }
+function newFile() {
+	console.log('This Are some changes made and Version 2.0');
+}
 function something() {
 	console.log('Iam something');
-
+}
 function something() {
 	console.log('Iam someting');
 	console.log('Bug Fixed');
@@ -13,3 +16,4 @@ function something() {
 console.log('Hello World');
 something();
 aBigNewFeature();
+newFile();
